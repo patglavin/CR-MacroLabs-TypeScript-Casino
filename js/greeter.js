f0d6d934d0b1bdb67c1b111df03af7c85bf81ccc
Patrick Glavin <patjglavin@gmail.com>
2018-03-23 16:43:18 -0400

    whoops didnt add

--- js/greeter.js
+++ js/greeter.js
@@ -1,10 +1,4 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = 'Pat';
 var el = document.getElementById('display');
-alert(el.id);
-// el.innerHTML = greeter(user);
-el.innerText = greeter(user);
-el.innerText += '\n';
-el.innerText += "ya big nerd";
+var btn = document.getElementById("submit");
+function addToInnerText() {
+}
