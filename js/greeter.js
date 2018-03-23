d711188049abd643cedec787efa9dfb77058ab58
Patrick Glavin <patjglavin@gmail.com>
2018-03-23 17:39:39 -0400

    this is the worst ide

--- js/greeter.js
+++ js/greeter.js
@@ -1,4 +1,10 @@
 var el = document.getElementById('display');
-var btn = document.getElementById("submit");
-function addToInnerText() {
+function addToDisplayText(text) {
+    el.innerText += '\n';
+    el.innerText += text;
 }
+var btn = document.getElementById('submit');
+var inputField = document.getElementById("user_input");
+// var entry = document.getElementById('user_input').innerText;
+btn.addEventListener("click", function (e) { return addToDisplayText(inputField.value); });
+//# sourceMappingURL=greeter.js.map
