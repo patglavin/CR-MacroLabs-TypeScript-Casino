6674319513d3548237be6f72dccb4d86494676f0
Patrick Glavin <patjglavin@gmail.com>
2018-03-23 15:00:26 -0400

    messing with innertext

--- js/greeter.js
+++ js/greeter.js
@@ -1,5 +1,11 @@
 function greeter(person) {
     return "Hello, " + person;
 }
-var user = 'Amy User';
-document.body.innerHTML = greeter(user);
+var user = 'Amy USER';
+var user1 = 'Amy USER';
+var el = document.getElementById('display');
+alert(el.id);
+// el.innerHTML = greeter(user);
+el.innerText = greeter(user);
+el.innerText += '\n';
+el.innerText += greeter(user1);
--- js/greeter.ts
+++ js/greeter.ts
@@ -3,9 +3,12 @@ function greeter(person) {
 }
 
 let user = 'Amy USER';
+let user1 = 'Amy USER';
 
 var el: HTMLElement = document.getElementById('display');
 alert(el.id);
 
 // el.innerHTML = greeter(user);
 el.innerText = greeter(user);
+el.innerText += '\n';
+el.innerText += greeter(user1);
