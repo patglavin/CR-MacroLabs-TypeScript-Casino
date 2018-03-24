c261cc1cb073ea9f159e965b9b2d3b6badb9eb41
Patrick Glavin <patjglavin@gmail.com>
2018-03-24 19:21:14 -0400

    prep for awfulTree

--- js/greeter.js
+++ js/greeter.js
@@ -1,16 +1,42 @@
 var el = document.getElementById('display');
-var userInput;
+let userInput;
+let currentGame = false;
+let updateInput = false;
+let userName;
+let userFunds;
 function addToDisplayText(text) {
-    el.innerText += '\n';
-    el.innerText += text;
+    if (text != '') {
+        el.innerText += '\n';
+        el.innerText += text;
+    }
 }
 function setInput(newInput) {
     userInput = newInput;
 }
 var btn = document.getElementById('submit');
+var startbtn = document.getElementById('start_game');
 var inputField = document.getElementById("user_input");
 // var entry = document.getElementById('user_input').innerText;
-btn.addEventListener("click", function (e) { return setInput(inputField.value); });
-btn.addEventListener("click", function (e) { return addToDisplayText(userInput); });
-btn.addEventListener("click", function (e) { return inputField.value = ''; });
+btn.addEventListener("click", (e) => setInput(inputField.value));
+btn.addEventListener("click", (e) => addToDisplayText(userInput));
+btn.addEventListener("click", (e) => inputField.value = '');
+btn.addEventListener("click", (e) => updateInput = true);
+startbtn.addEventListener("click", (e) => currentGame = true);
+startbtn.addEventListener("click", (e) => blackJackTurn());
+function blackJackTurn() {
+    let blackjack = new BlackJack();
+    blackjack.askForName();
+}
+var timeout = null;
+function acceptInput() {
+}
+class BlackJack {
+    constructor() { }
+    askForName() {
+        addToDisplayText('write your name');
+        btn.addEventListener("click", (e) => this.name = inputField.value);
+        addToDisplayText(this.name);
+    }
+}
+blackJackTurn();
 //# sourceMappingURL=greeter.js.map
--- ts/greeter.ts
+++ ts/greeter.ts
@@ -1,9 +1,15 @@
 var el: HTMLElement = document.getElementById('display');
 let userInput;
+let currentGame = false;
+let updateInput = false;
+let userName:string;
+let userFunds:number;
 
 function addToDisplayText(text: string) {
-    el.innerText += '\n';
-    el.innerText += text;
+    if (text != '') {
+        el.innerText += '\n';
+        el.innerText += text;
+    }
 }
 
 function setInput(newInput:string) {
@@ -11,9 +17,37 @@ function setInput(newInput:string) {
 }
 
 var btn = document.getElementById('submit');
+var startbtn = document.getElementById('start_game')
 var inputField: HTMLInputElement = <HTMLInputElement>document.getElementById("user_input");
 // var entry = document.getElementById('user_input').innerText;
 btn.addEventListener("click", (e: Event) => setInput(inputField.value));
 btn.addEventListener("click", (e:Event) => addToDisplayText(userInput));
 btn.addEventListener("click", (e: Event) => inputField.value = '');
+btn.addEventListener("click", (e: Event) => updateInput = true);
+startbtn.addEventListener("click", (e: Event) => currentGame = true);
+startbtn.addEventListener("click", (e:Event) => blackJackTurn());
 
+function blackJackTurn() {
+    let blackjack = new BlackJack();
+    blackjack.askForName();
+}
+
+var timeout = null;
+
+function acceptInput() {
+    
+} 
+
+class BlackJack {
+    constructor() {}
+    name:string;
+    askForName() {
+        addToDisplayText('write your name');
+        btn.addEventListener("click", (e:Event) => this.name = inputField.value);
+        
+        addToDisplayText(this.name);
+    }
+    
+}
+
+blackJackTurn();
