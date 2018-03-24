224a6e466af786346eb3ba3d81522924197a0dc4
Patrick Glavin <patjglavin@gmail.com>
2018-03-24 19:54:50 -0400

    adjust css, add suit and value enums

--- js/greeter.js
+++ js/greeter.js
@@ -1,7 +1,6 @@
 var el = document.getElementById('display');
 let userInput;
 let currentGame = false;
-let updateInput = false;
 let userName;
 let userFunds;
 function addToDisplayText(text) {
@@ -18,24 +17,67 @@ var startbtn = document.getElementById('start_game');
 var inputField = document.getElementById("user_input");
 // var entry = document.getElementById('user_input').innerText;
 btn.addEventListener("click", (e) => setInput(inputField.value));
-btn.addEventListener("click", (e) => addToDisplayText(userInput));
-btn.addEventListener("click", (e) => inputField.value = '');
-btn.addEventListener("click", (e) => updateInput = true);
-startbtn.addEventListener("click", (e) => currentGame = true);
-startbtn.addEventListener("click", (e) => blackJackTurn());
+btn.addEventListener("click", (e) => addToDisplayText('you input ' + userInput));
+function clearInput() {
+    inputField.value = '';
+}
 function blackJackTurn() {
     let blackjack = new BlackJack();
-    blackjack.askForName();
+    btn.addEventListener("click", (e) => blackjack.updateInput());
 }
 var timeout = null;
 function acceptInput() {
 }
+var CardValue;
+(function (CardValue) {
+    CardValue[CardValue["Two"] = 2] = "Two";
+    CardValue[CardValue["Three"] = 3] = "Three";
+    CardValue[CardValue["Four"] = 4] = "Four";
+    CardValue[CardValue["Five"] = 5] = "Five";
+    CardValue[CardValue["Six"] = 6] = "Six";
+    CardValue[CardValue["Seven"] = 7] = "Seven";
+    CardValue[CardValue["Eight"] = 8] = "Eight";
+    CardValue[CardValue["Nine"] = 9] = "Nine";
+    CardValue[CardValue["Ten"] = 10] = "Ten";
+    CardValue[CardValue["Jack"] = 10] = "Jack";
+    CardValue[CardValue["Queen"] = 10] = "Queen";
+    CardValue[CardValue["King"] = 10] = "King";
+    CardValue[CardValue["Ace"] = 11] = "Ace";
+})(CardValue || (CardValue = {}));
+var Suit;
+(function (Suit) {
+    Suit[Suit["Hearts"] = 0] = "Hearts";
+    Suit[Suit["Diamonds"] = 1] = "Diamonds";
+    Suit[Suit["Clubs"] = 2] = "Clubs";
+    Suit[Suit["Spades"] = 3] = "Spades";
+})(Suit || (Suit = {}));
+class card {
+    constructor(parameters) {
+    }
+}
+class deck {
+    constructor() {
+    }
+}
 class BlackJack {
-    constructor() { }
-    askForName() {
-        addToDisplayText('write your name');
-        btn.addEventListener("click", (e) => this.name = inputField.value);
-        addToDisplayText(this.name);
+    constructor() {
+        this.turnOrder = 0;
+    }
+    updateInput() {
+        if (this.turnOrder == 0) {
+            this.initialTurn();
+        }
+        else if (this.turnOrder == 1) {
+            alert('turn 1');
+            addToDisplayText(this.name);
+            this.turnOrder += 1;
+        }
+    }
+    initialTurn() {
+        alert('turn 0');
+        this.name = inputField.value;
+        clearInput();
+        this.turnOrder += 1;
     }
 }
 blackJackTurn();
--- ts/greeter.ts
+++ ts/greeter.ts
@@ -1,7 +1,6 @@
 var el: HTMLElement = document.getElementById('display');
 let userInput;
 let currentGame = false;
-let updateInput = false;
 let userName:string;
 let userFunds:number;
 
@@ -21,15 +20,15 @@ var startbtn = document.getElementById('start_game')
 var inputField: HTMLInputElement = <HTMLInputElement>document.getElementById("user_input");
 // var entry = document.getElementById('user_input').innerText;
 btn.addEventListener("click", (e: Event) => setInput(inputField.value));
-btn.addEventListener("click", (e:Event) => addToDisplayText(userInput));
-btn.addEventListener("click", (e: Event) => inputField.value = '');
-btn.addEventListener("click", (e: Event) => updateInput = true);
-startbtn.addEventListener("click", (e: Event) => currentGame = true);
-startbtn.addEventListener("click", (e:Event) => blackJackTurn());
+btn.addEventListener("click", (e:Event) => addToDisplayText('you input ' + userInput));
+
+function clearInput() {
+    inputField.value = '';
+}
 
 function blackJackTurn() {
     let blackjack = new BlackJack();
-    blackjack.askForName();
+    btn.addEventListener("click", (e: Event) => blackjack.updateInput());
 }
 
 var timeout = null;
@@ -38,16 +37,66 @@ function acceptInput() {
     
 } 
 
-class BlackJack {
-    constructor() {}
-    name:string;
-    askForName() {
-        addToDisplayText('write your name');
-        btn.addEventListener("click", (e:Event) => this.name = inputField.value);
+enum CardValue {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 10,
+    Queen = 10,
+    King = 10,
+    Ace = 11,
+}
+
+enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+class card {
+    value:CardValue;
+    suit:string;
+    constructor(parameters) {
         
-        addToDisplayText(this.name);
     }
-    
+}
+
+class deck {
+    cards;
+
+    constructor() {
+        
+    }
+}
+
+class BlackJack {
+    constructor() {
+
+    }
+    turnOrder:number = 0;
+    name:string;
+    updateInput(){
+        if (this.turnOrder == 0) {
+            this.initialTurn();
+        } else if (this.turnOrder == 1) {
+            alert('turn 1');
+            addToDisplayText(this.name);
+            this.turnOrder += 1;
+        }   
+    }
+    initialTurn(){
+        alert('turn 0');
+        this.name = inputField.value;
+        clearInput();
+        this.turnOrder += 1;
+    }
 }
 
 blackJackTurn();
